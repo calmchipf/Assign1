b95dca5dc58cd4fb780aa9b53ab6d9396330e1d4
calmchipf <dk123tm@mail.ru>
2024-01-27 16:28:29 +0600

    Added comments to the main class for clarity

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -9,20 +9,20 @@ public class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Users\\Dos\\Assign1\\src\\newsource.txt");
 
-        Scanner sc = new Scanner(file);
-        Shape shape = new Shape();
+        Scanner sc = new Scanner(file);  // Allocates memory for the scanner of our file
+        Shape shape = new Shape();  // Creates an instance of Shape class
 
-        while (sc.hasNext()) {
-            double x = sc.nextDouble();
-            double y = sc.nextDouble();
+        while (sc.hasNext()) { // Scans the file until the last input token
+            double x = sc.nextDouble();  // Assigns the next double for x
+            double y = sc.nextDouble();  // Assigns the next double for y
 
-            Point point = new Point(x, y);
-            shape.addPoint(point);
+            Point point = new Point(x, y);  // Creates an instance of a point Class with x and y as parameters
+            shape.addPoint(point);  // Adds the instance to the array of the shape class' object
         }
 
-        System.out.println(shape.calculatePerimeter());
-        System.out.println(shape.getLongestSide());
-        System.out.println(shape.getAverageSide());
+        System.out.println(shape.calculatePerimeter() + " - The perimeter of the shape.");
+        System.out.println(shape.getLongestSide() + " - The longest side of the shape.");
+        System.out.println(shape.getAverageSide() + " - The average side of the shape.");
 
     }
 }
