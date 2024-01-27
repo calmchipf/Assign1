aa74aa125cf07e6804d78c4eac9455924e680d4f
calmchipf <dk123tm@mail.ru>
2024-01-27 15:10:35 +0600

    Initial Commit

--- src/models/Point.java
+++ src/models/Point.java
@@ -1,12 +1,38 @@
 package models;
+import java.lang.Math;
 
 public class Point {
     private double x;
     private double y;
 
-    // constructor having both parameters
+    public Point(double x-cord, double y-cord){
+        setX(x-cord);
+        setY(y-cord);
+    }
 
-    // distance(Point dest) - distance from current point to the dest point
+    public void setX(double x-cord) {
+        this.x = x-cord;
+    }
 
-    // toString
+    public void setY(double y-cord) {
+        this.y = y-cord;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public distance(Point dest) {
+        xside = dest.x - this.x;
+        yside = dest.y - this.y;
+        return Math.sqrt(Math.pow(xside, 2) + Math.pow(yside, 2))
+    }
+
+    public String toString {
+        return "Point = { x: " + this.getX() + ", y: " + this.getY() + " }";
+    }
 }
