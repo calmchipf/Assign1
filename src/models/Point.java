a4ccb0c3328e6180ba18392f7b5d71b01fffb917
calmchipf <dk123tm@mail.ru>
2024-01-27 16:37:44 +0600

    Added comments to the Point class and Shape class for clarity

--- src/models/Point.java
+++ src/models/Point.java
@@ -2,37 +2,37 @@ package models;
 import java.lang.Math;
 
 public class Point {
-    private double x;
-    private double y;
+    private double x;  // Attribute for x-coordinate
+    private double y;  // Attribute for y-coordinate
 
-    public Point(double x_cord, double y_cord){
-        setX(x_cord);
-        setY(y_cord);
+    public Point(double x_cord, double y_cord){  // Parametrized constructor taking the x and y coordinate values
+        setX(x_cord);  // Call of a setter for x-coordinate
+        setY(y_cord);  // Call of a setter for y-coordinate
     }
 
     public double getX() {
         return x;
-    }
+    }  // Getter for x-coordinate
 
     public void setX(double x_cord) {
         this.x = x_cord;
-    }
+    }  // Setter for x-coordinate
 
     public double getY() {
         return y;
-    }
+    }  // Getter for x-coordinate
 
     public void setY(double y_cord) {
         this.y = y_cord;
+    }  // Setter for y-coordinate
+
+    public double distance(Point dest_point) {  // Method for calculating the distance between points
+        double xside = dest_point.x - this.x;  // Difference between the x-coordinates
+        double yside = dest_point.y - this.y;  // Difference between the y-coordinates
+        return Math.sqrt(Math.pow(xside, 2) + Math.pow(yside, 2));  // The distance between coordinates
     }
 
-    public double distance(Point dest_point) {
-        double xside = dest_point.x - this.x;
-        double yside = dest_point.y - this.y;
-        return Math.sqrt(Math.pow(xside, 2) + Math.pow(yside, 2));
-    }
-
-    public String toString() {
+    public String toString() {  // Method for returning the values as a string
         return "Point = { x: " + this.getX() + ", y: " + this.getY() + " }";
     }
 
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -2,40 +2,43 @@ package models;
 import java.util.ArrayList;
 public class Shape {
 
-    ArrayList<Point> points;
-    public Shape(){
+    ArrayList<Point> points;  // Creating an attribute for the ArrayLis
+    public Shape(){  // A default constructor allocating memory for our 'points' attribute
         points = new ArrayList<Point>();
     }
 
-    public void addPoint(Point point){
+    public void addPoint(Point point){  // Method for adding the given point to the array
         points.add(point);
     }
 
-    public double calculatePerimeter(){
-        double perimeter = 0;
+    public double calculatePerimeter(){  // Method for calculating the perimeter of the shape
+        double perimeter = 0;  // Declaration of the perimeter
         int length = points.size();
 
+        // Adding the length between the first and last points to the perimeter
         perimeter += points.getFirst().distance(points.get(length - 1));
-        for (int i = 1; i < length; i++) {
+        for (int i = 1; i < length; i++) {  // Adds the lengths between points from first to last to the perimeter
             perimeter += points.get(i - 1).distance(points.get(i));
         }
 
-        return perimeter;
+        return perimeter;  // Return of the perimeter
     }
 
-    public double getAverageSide() {
-        return calculatePerimeter() / points.size();
+    public double getAverageSide() {  // Method for calculating the average side of the shape
+        return calculatePerimeter() / points.size();  // Return of the perimeter divided by the number of points
     }
 
-    public double getLongestSide() {
+    public double getLongestSide() {  // Method for getting the longest side of the shape
         int length = points.size();
         double max_side = points.getFirst().distance(points.get(length - 1));
+        // By typing this we assume that the first length is the maximum
 
-        for (int i = 1; i < length; i++) {
+        for (int i = 1; i < length; i++) {  // We check every other length with the initial to get the maximum
             double next_side = points.get(i - 1).distance(points.get(i));
             if (next_side > max_side) max_side = next_side;
         }
 
-        return max_side;
+        return max_side;  // Return of the longest side
     }
+
 }
