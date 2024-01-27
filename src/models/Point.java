5330b3948bddcbce4e02f3a3c4f2c01c45cddc29
calmchipf <dk123tm@mail.ru>
2024-01-27 16:05:26 +0600

    Implemented Shape class and fixed Point class variable names

--- src/models/Point.java
+++ src/models/Point.java
@@ -5,17 +5,17 @@ public class Point {
     private double x;
     private double y;
 
-    public Point(double x-cord, double y-cord){
-        setX(x-cord);
-        setY(y-cord);
+    public Point(double x_cord, double y_cord){
+        setX(x_cord);
+        setY(y_cord);
     }
 
-    public void setX(double x-cord) {
-        this.x = x-cord;
+    public void setX(double x_cord) {
+        this.x = x_cord;
     }
 
-    public void setY(double y-cord) {
-        this.y = y-cord;
+    public void setY(double y_cord) {
+        this.y = y_cord;
     }
 
     public double getX() {
@@ -26,13 +26,13 @@ public class Point {
         return y;
     }
 
-    public distance(Point dest) {
-        xside = dest.x - this.x;
-        yside = dest.y - this.y;
-        return Math.sqrt(Math.pow(xside, 2) + Math.pow(yside, 2))
+    public double distance(Point dest_point) {
+        double xside = dest_point.x - this.x;
+        double yside = dest_point.y - this.y;
+        return Math.sqrt(Math.pow(xside, 2) + Math.pow(yside, 2));
     }
 
-    public String toString {
+    public String toString() {
         return "Point = { x: " + this.getX() + ", y: " + this.getY() + " }";
     }
 
--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,13 +1,41 @@
 package models;
-
+import java.util.ArrayList;
 public class Shape {
-    // container of Points (e.g. ArrayList<Point>)
 
-    // addPoint(Point) - adds to the container
+    ArrayList<Point> points;
+    public Shape(){
+        points = new ArrayList<Point>();
+    }
 
-    // calculatePerimeter()
+    public void addPoint(Point point){
+        points.add(point);
+    }
 
-    // getAverageSide()
+    public double calculatePerimeter(){
+        double perimeter = 0;
+        int length = points.size();
 
-    // getLongestSide()
+        perimeter += points.getFirst().distance(points.get(length - 1));
+        for (int i = 1; i < length; i++) {
+            perimeter += points.get(i - 1).distance(points.get(i));
+        }
+
+        return perimeter;
+    }
+
+    public double getAverageSide() {
+        return calculatePerimeter() / points.size();
+    }
+
+    public double getLongestSide() {
+        int length = points.size();
+        double max_side = points.getFirst().distance(points.get(length - 1));
+
+        for (int i = 1; i < length; i++) {
+            double next_side = points.get(i - 1).distance(points.get(i));
+            if (next_side > max_side) max_side = next_side;
+        }
+
+        return max_side;
+    }
 }
